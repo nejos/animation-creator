aa60094bbd51abe8565260dab303b96ce4e05eb7
Szymon Nitecki <szymon.nn@gmail.com>
2018-05-17 15:49:08 +0000

    added name generator and test for it

---
+++ tests/test_name_generator.py
@@ -0,0 +1,12 @@
+import unittest
+from media.generate_name import generate_name
+import re
+
+class TestNameGenerator(unittest.TestCase):
+
+  def test_generation_of_unique_name(self):
+    n1 = generate_name('gamma.jpg')
+    n2 = generate_name('gamma.jpg')
+    regexp = re.compile(r'^uploads\/[0-9a-f-]{36}\/gamma\.jpg$')
+    assert n1 != n2
+    assert regexp.search(n1)
